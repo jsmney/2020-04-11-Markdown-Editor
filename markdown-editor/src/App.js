30160e2f10a704b7a73dcc5e1cb0bd65380a4113
Jasmine Wang <jasminey.tea@gmail.com>
2020-04-11 14:15:34 -0500

    added functionalities last minute

--- markdown-editor/src/App.js
+++ markdown-editor/src/App.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
-import { Container, Header, Grid, Form, TextArea } from 'semantic-ui-react'
+import {
+  Container,
+  Header,
+  Grid,
+  Form,
+  TextArea,
+  Button
+} from 'semantic-ui-react'
 import './App.css'
 import MarkdownIt from 'markdown-it'
 const md = new MarkdownIt()
@@ -7,10 +14,21 @@ const md = new MarkdownIt()
 function App() {
   const [input, setInput] = useState('')
   const [markedDownText, setMarkedDownText] = useState('')
+
   const handleChange = (evt) => {
     // updates input, and formats markdown side of text
     setInput(evt.target.value)
     format(evt.target.value)
+  }
+
+  const handleClear = (evt) => {
+    setInput('')
+    setMarkedDownText('')
+  }
+
+  const handleCopy = (evt) => {
+    navigator.clipboard.writeText(markedDownText)
+    evt.target.focus()
   }
 
   const format = (str) => {
@@ -37,9 +55,22 @@ function App() {
                 onChange={handleChange}
               ></TextArea>
             </Form>
+            <Button onClick={handleClear}>Clear</Button>
           </Grid.Column>
           <Grid.Column>
+            <Header as="h3">Formatted Text Preview</Header>
             <div dangerouslySetInnerHTML={{ __html: markedDownText }}></div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column></Grid.Column>
+          <Grid.Column>
+            <Header as="h3">Formatted Text as HTML</Header>
+            <TextArea
+              value={markedDownText}
+              style={{ minHeight: 250, minWidth: '100%' }}
+            ></TextArea>
+            <Button onClick={handleCopy}>Copy</Button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
